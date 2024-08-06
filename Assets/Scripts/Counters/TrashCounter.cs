cb0fae5db0fdd5b15158a99bcf59f7214f808690
Feli <feliciabeatrice7@gmail.com>
2024-08-06 14:01:20 +0800

    trash counter

--- Assets/Scripts/Counters/TrashCounter.cs
+++ Assets/Scripts/Counters/TrashCounter.cs
@@ -2,17 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TrashCounter : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+public class TrashCounter : BaseCounter {
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    public override void Interact(Player player) {
+        if (player.HasKitchenObject()) {
+            player.GetKitchenObject().DestroySelf();
+        }
     }
+    
 }
